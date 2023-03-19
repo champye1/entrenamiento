c7afb7aa0bcc3bca59f96039e52c91e3f2e70f4b
LoneyMan1 <estebanv144@gmail.com>
2023-03-19 20:13:22 -0300

    entrenamiento de tipo

---
+++ Datatype.py
@@ -0,0 +1,46 @@
+#string
+
+print("hello world")
+print('hello world')
+print("""hello world """)
+print('''hello world''')
+
+# para mostrar el tipo de dato
+print (type("hello world"))
+
+
+#concatenar dos string
+
+print("bye" + "world")
+
+# para un numero entero se cataloga como int ejemplo 30
+print(30)
+# para un numero con decimal se cataloga como un float ejemplo 30.1
+print(30.513)
+
+#boolean
+True
+False
+print(type(True))
+print(type(False))
+
+#lista muteable corchete
+
+print(type([10,20,30,55]))
+print([10,False,True,55])
+
+#tupla es una lista la cual no se puede cambiar # para las tuplas son entre parentesis
+print((10,40,50,102))
+
+#diccionario
+#mostrar dato
+print({"name":"ryan",
+        "apellido": "ray",
+      "sobre nombre":"fazt"})
+
+#mostrar tipo de dato
+print(type({"name":"ryan",
+        "apellido": "ray",
+      "sobre nombre":"fazt"}))
+
+      #min 51 
